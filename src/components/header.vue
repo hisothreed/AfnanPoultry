c528fbf02918673686bd8030e70b955a0b7a547b
Hiso3D <Hiso3D@Hiso3Ds-iMac.local>
2017-07-28 15:29:07 +0300

    .

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="headerContainer">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "headerContainer",
+  data: () => ({
+
+  })
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="mainViewContainer">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "mainViewContainer",
+  data: () => ({
+
+  })
+}
+</script>
+<style scoped>
+</style>
